d55b09dcd15a2f566489e659c4e365ac922616b9
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-02-18 17:28:08 -0600

    add basic entities for DbContext

---
+++ ShoppingOL/ShoppingOL/Data/CustomDBContext.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingOL.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingOL.Data
+{
+    public class CustomDBContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
