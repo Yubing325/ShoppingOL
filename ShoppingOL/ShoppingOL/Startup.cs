b6255f19a95b96d6030ac5323e8d38554a3cc7b6
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-02-19 19:57:43 -0600

    init the CustomDBContext configuration in ConfigureServices. 1/2

--- ShoppingOL/ShoppingOL/Startup.cs
+++ ShoppingOL/ShoppingOL/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ShoppingOL.Data;
 
 namespace ShoppingOL
 {
@@ -15,6 +17,9 @@ namespace ShoppingOL
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CustomDBContext>(cfg => {
+                cfg.UseSqlServer("");
+            });
             services.AddMvc();
         }
 
