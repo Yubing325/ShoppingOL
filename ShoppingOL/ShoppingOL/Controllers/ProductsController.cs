a491836a987d7c4fb9bbd2b7bf530d01b39093d2
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-02-23 07:13:37 -0600

    Init ProductsController

---
+++ ShoppingOL/ShoppingOL/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShoppingOL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingOL.Controllers
+{
+    public class ProductsController: Controller
+    {
+        private readonly IShoppingRepository shoppingRepo;
+        private readonly ILogger<ProductsController> logger;
+
+        public ProductsController(IShoppingRepository shoppingRepo, ILogger<ProductsController> logger)
+        {
+            this.shoppingRepo = shoppingRepo;
+            this.logger = logger;
+        }
+
+
+    }
+}
