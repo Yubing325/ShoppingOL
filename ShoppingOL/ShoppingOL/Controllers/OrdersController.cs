07874716078b8c3356c0aa452a1bb14124ef8d29
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-03-06 07:03:36 -0600

    Create OrdersController

---
+++ ShoppingOL/ShoppingOL/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingOL.Data;
+using ShoppingOL.Data.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace ShoppingOL.Controllers
+{
+    [Route("api/[Controller]")]
+    public class OrdersController : Controller
+    {
+        private readonly IShoppingRepository repository;
+        private readonly ILogger<OrdersController> logger;
+
+        public OrdersController(IShoppingRepository repository, ILogger<OrdersController> logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(repository.GetAllOrders());
+            }
+            catch (Exception ex)
+            {
+
+                logger.LogError("Failed to get orders: {0}", ex);
+                return BadRequest("Failed to get orders.");
+            }
+        }
+    }
+}
